4b799083befdc71d27463183fd68b4a00d643b24
crohnaro <crohnarodev@gmail.com>
2023-05-29 16:08:36 -0300

    feat: :lipstick: creating the header component

---
+++ capputeeno/src/components/primary-input.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PrimaryInput = styled.input`
+    width: 352px;
+    border: none;
+    border-radius: 8px;
+    background-color: var(--bg-secondary);
+    padding: 10px 16px;
+    font-family: inherit;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 22px;
+    color: var(--text-dark);
+`
