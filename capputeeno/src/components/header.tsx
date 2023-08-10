34cf6a02eb8e15bc2217290b3b69dd46d5d1f5c1
crohnaro <crohnarodev@gmail.com>
2023-08-10 09:51:18 -0300

    feat: :sparkles: adding search functionality to the input search

--- capputeeno/src/components/header.tsx
+++ capputeeno/src/components/header.tsx
@@ -1,49 +1,53 @@
-"use client"
+"use client";
 
-import styled from "styled-components"
+import styled from "styled-components";
 
-import { Saira_Stencil_One } from 'next/font/google'
-import { PrimaryInput, PrimaryInputWithSearchIcon } from "./primary-input"
-import { CartControl } from "./cart-control"
+import { Saira_Stencil_One } from "next/font/google";
+import { PrimaryInput, PrimaryInputWithSearchIcon } from "./primary-input";
+import { CartControl } from "./cart-control";
+import { useFilter } from "@/hooks/usefilter";
 
-const sairaStencil = Saira_Stencil_One({ 
-  weight: [ '400' ],
-  subsets: ['latin'] 
-})
+const sairaStencil = Saira_Stencil_One({
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
-interface HeaderProps{
-
-}
+interface HeaderProps {}
 
 const TagHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 160px;
+
+  > div {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    padding: 20px 160px;
-
-    > div {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 24px;
-    }
-`
+    justify-content: center;
+    gap: 24px;
+  }
+`;
 
 const Logo = styled.a`
-    color: var(--logo-color);
-    font-weight: 400;
-    font-size: 40px;
-    line-height: 150%;
-`
+  color: var(--logo-color);
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 150%;
+`;
 
-export function Header(props: HeaderProps){
-    return(
-        <TagHeader>
-            <Logo className={sairaStencil.className}>Capputeeno</Logo>
-            <div>
-                <PrimaryInputWithSearchIcon placeholder="Procurando por algo específico?"/>
-                <CartControl />
-            </div>
-        </TagHeader>
-    )
+export function Header(props: HeaderProps) {
+  const { setSearch, search } = useFilter();
+  return (
+    <TagHeader>
+      <Logo className={sairaStencil.className}>Capputeeno</Logo>
+      <div>
+        <PrimaryInputWithSearchIcon
+          value={search}
+          handleChange={setSearch}
+          placeholder="Procurando por algo específico?"
+        />
+        <CartControl />
+      </div>
+    </TagHeader>
+  );
 }
