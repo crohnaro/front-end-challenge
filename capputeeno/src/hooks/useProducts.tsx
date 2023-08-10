b877ee0c88d4f8a41a02f6b79300c75190cba8cf
crohnaro <crohnarodev@gmail.com>
2023-08-10 09:52:55 -0300

    refactor: :art: refactor the code to the greater legibility

--- capputeeno/src/hooks/useProducts.tsx
+++ capputeeno/src/hooks/useProducts.tsx
@@ -2,47 +2,30 @@ import { ProductsFetchResponse } from "@/types/products-response";
 import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios";
 import { useFilter } from "./usefilter";
-import { FilterType } from "@/types/filter-types";
-import { getCategoryByType } from "@/utils/get-category-by-type";
+import {  mountQuery } from "@/utils/graphql-filters";
+import { useDeferredValue } from "react";
+
 
 const fetcher = (query: string): AxiosPromise<ProductsFetchResponse> => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "https://localhost:3333";
   return axios.post(apiUrl, { query });
 };
 
-const mountQuery = (type: FilterType) => {
-  if(type === FilterType.ALL) return `
-  query {
-    allProducts {
-      id
-      name
-      price_in_cents
-      image_url
-    }
-  }
-` 
-return `
-  query {
-    allProducts(filter: { category: "${getCategoryByType(type)}" }) {
-      id
-      name
-      price_in_cents
-      image_url
-      category
-    }
-  }
-`
-}
+
 
 export function useProducts() {
-  const { type } = useFilter()
-  const query = mountQuery(type)
+  const { type, priority, search } = useFilter()
+  const searchDeferred = useDeferredValue(search);
+  const query = mountQuery(type, priority);
   const { data } = useQuery({
     queryFn: () => fetcher(query),
-    queryKey: ["products", type],
+    queryKey: ["products", type, priority],
   });
 
+  const products = data?.data?.data?.allProducts
+  const filteredProducts = products?. filter(product => product.name.toLowerCase().includes(searchDeferred.toLowerCase()))
+
   return {
-    data: data?.data?.data?.allProducts,
+    data: filteredProducts,
   };
 }
